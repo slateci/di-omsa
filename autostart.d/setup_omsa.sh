80f5ba74a07e458fd9508d5af8fa1773e4f6bb70
Emerson Ford <emersontford@gmail.com>
2019-10-22 13:54:05 -0600

    Improving setup_omsa to allow dsu to run properly

--- autostart.d/setup_omsa.sh
+++ autostart.d/setup_omsa.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+echo " * Setting releasever yum variable"
+source /etc/os-release
+echo $VERSION_ID >> /etc/yum/vars/releasever
+
+echo " * Running Dell bootstrap to get repos"
+dell_bootstrap.sh
+
+yum install centos-release -y
+# yum install srvadmin-base srvadmin-server-cli srvadmin-storage-cli -y
+
 if [[ -f /opt/dell/srvadmin/sbin/srvadmin-services.sh ]]; then
   echo " * setting up Dell GPG keys"
   curl -s https://linux.dell.com/repo/hardware/dsu/copygpgkeys.sh | bash
